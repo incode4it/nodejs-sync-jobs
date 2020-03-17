9bed88968cf1f1dd276252451acef8313aa3aca7
Ion <nelu.bulat@hotmail.com>
2020-03-17 22:36:00 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+const myEmitter = new EventEmitter();
+let isJobCurrentlyRunning = false;
+
+async function doAsyncJobsOneByOne(id) {
+    if (!isJobCurrentlyRunning) {
+        doJob(id);
+    } else {
+        await once(myEmitter, 'jobdone');
+        doJob(id);
+    }
+}
+
+function doJob(id) {
+    isJobCurrentlyRunning = true;
+    setTimeout(() => {
+        console.log('done', id)
+        isJobCurrentlyRunning = false;
+        myEmitter.emit('jobdone')
+    }, randInt(1000))
+}
+
+for (let i = 0; i < 3; i++) {
+    setTimeout((() => {
+        doAsyncJobsOneByOne(i + 1)
+    }).bind(i))
+}
+console.log('binded 3')
